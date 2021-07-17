8cd0c6a6e3a46fef78865d400590039fb54d6149
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2021-07-17 13:48:17 +0100

    Added local Storage function to load items in storage when page loads. Added convertImageToBlob function to convert images to blob and store in localStorage. Finally, I edited the cart css images to be in cart.

--- drag-and-drop.js
+++ drag-and-drop.js
@@ -9,5 +9,28 @@ function drag(event) {
 function drop(event) {
     event.preventDefault();
     let data = document.getElementById(event.dataTransfer.getData("text"));
-    data.appendChild(data);
+    let nodes = [...data.parentNode.children];
+
+    convertImage(data.children[0], nodes.findIndex((ele) => ele === data));
+    let dropzone = document.querySelector('.drop-zone');
+    let imgsInDropZone = [...document.querySelectorAll('.drop-zone img')];
+    if(imgsInDropZone.findIndex((ele) => data.children[0].style.getPropertyValue('--index') == ele.style.getPropertyValue('--index')) == -1){
+        dropzone.appendChild(data.children[0]);
+    }
+}
+
+function convertImage(image, num) {
+    const reader = new FileReader();
+
+    fetch(image.getAttribute("src"))
+        .then(function (response) {
+            return response.blob()
+        })
+        .then(function (blob) {
+            image = blob
+            reader.readAsDataURL(image)
+        });
+        reader.addEventListener("load", () => {
+            localStorage.setItem(`img-${num+1}`, reader.result);
+        })
 }
