e9e504faf0556c357e0d1957109cc9b07155965e
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2021-07-19 22:08:58 +0100

    added image files and item count to localStorage

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 window.onload = () => {
     Object.keys(localStorage).forEach((key, index) => {
-        let dropzone = document.querySelector('.drop-zone');
-        let img = document.createElement('img');
-        img.setAttribute('src', blobToFile(localStorage.getItem(key), String(key)));
-        img.setAttribute('style', `--index: ${index+1}`);
-        dropzone.appendChild(img);
+        if(key.substring(0, 3) == 'img'){
+            let dropzone = document.querySelector('.drop-zone');
+            let img = document.createElement('img');
+            img.setAttribute('src', blobToFile(localStorage.getItem(key), String(key)));
+            img.setAttribute('style', `--index: ${parseInt(key.substring(4, 5))}`);
+            dropzone.appendChild(img);
+        }
+        if(key == 'total-items'){
+            let itemNum = document.querySelector('#item-num');
+            itemNum.textContent = JSON.parse(localStorage.getItem(key));
+        }
+        if(key.substring(0, 2) == 'id'){
+            let deleteItemSection = document.querySelectorAll('.item-drop-down')[1];
+            deleteItemSection.children[parseInt(key.substring(3, 4))].children[0].textContent = `x ${JSON.parse(localStorage.getItem(key))}`;
+        }
     })
-    
 }
 
 function saveName(element){
@@ -28,4 +37,28 @@ function getCookie(name) {
     }
 
     return null;
+}
+
+// Add items to DOM
+function addItem(item){
+    let allItemsNums = document.querySelectorAll('.items .item-counter');
+    let tempArrStrings = [];
+    for (let i = 0; i < allItemsNums.length; i++) {
+        const element = allItemsNums[i];
+        const elementText = element.parentNode.textContent;
+        const elementToCheck = elementText.substring(0, elementText.indexOf("x")).trim().toLocaleLowerCase();
+        if(item.textContent.toLocaleLowerCase() == elementToCheck){
+            tempArrStrings = element.textContent.split(' ');
+            if(parseInt(tempArrStrings[1]) + 1 > 20){
+                break;
+            }
+            tempArrStrings[1] = String(parseInt(tempArrStrings[1]) + 1);
+            element.textContent = `${tempArrStrings[0]} ${tempArrStrings[1]}`;
+            break;
+        } 
+    }
+}
+
+function toggleItems(element){
+    element.parentNode.nextElementSibling.classList.toggle('hide');
 }
