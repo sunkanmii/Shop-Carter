ed102452c614106fecd0997c57f2c5071a073466
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2021-07-20 19:00:09 +0100

    added addItemToCart function to handle adding items to cart when item is clicked and edited css

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -11,48 +11,10 @@ function drop(event) {
     let data = document.getElementById(event.dataTransfer.getData("text"));
     let nodes = [...data.parentNode.children];
     let indOfSection = nodes.findIndex((ele) => ele == data);
-
     convertImageToBlob(data.children[0].cloneNode(), indOfSection);
-    let dropzone = document.querySelector('.drop-zone');
-    let imgsInDropZone = [...document.querySelectorAll('.drop-zone img')];
-
-    if (imgsInDropZone.findIndex((ele) => data.children[0].style.getPropertyValue('--index') == ele.style.getPropertyValue('--index')) == -1) {
-        dropzone.appendChild(data.children[0].cloneNode());
-    }
-
-    let itemCount = parseInt(data.children[1].children[0].textContent.substring(2, 3));
-    let idOfItem = JSON.parse(localStorage.getItem(`id-${indOfSection}`));
-
-    // Save item count in localstorage
-    if (idOfItem != null) {
-        localStorage.setItem(`id-${indOfSection}`, itemCount + idOfItem);
-    } else {
-        localStorage.setItem(`id-${indOfSection}`, itemCount);
-    }
-
-    let deleteItemSection = document.querySelectorAll('.item-drop-down')[1];
-
-    deleteItemSection.children[indOfSection].children[0].textContent = `x ${JSON.parse(localStorage.getItem(`id-${indOfSection}`))}`;
-    let totalItems = document.querySelector('#item-num');
-    totalItems.textContent = parseInt(totalItems.textContent) + parseInt(data.children[1].querySelector('.item-counter').textContent.substring(1, 3));
-    localStorage.setItem('total-items', totalItems.textContent);
-
+    // Add item to cart
+    addItemToCart(data);
     // popup
     popUp();
 }
 
-function convertImageToBlob(image, num) {
-    const reader = new FileReader();
-
-    fetch(image.getAttribute("src"))
-        .then(function (response) {
-            return response.blob()
-        })
-        .then(function (blob) {
-            image = blob
-            reader.readAsDataURL(image)
-        });
-    reader.addEventListener("load", () => {
-        localStorage.setItem(`img-${num+1}`, reader.result);
-    })
-}
